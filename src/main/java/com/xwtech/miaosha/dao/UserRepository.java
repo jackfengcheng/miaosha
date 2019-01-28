9074036a9afacac6b0b53136c67f0d741f4bce99
冯成 <531739331@qq.com>
2019-01-28 11:16:52 +0800

    jpa   query查询

--- src/main/java/com/xwtech/miaosha/dao/UserRepository.java
+++ src/main/java/com/xwtech/miaosha/dao/UserRepository.java
@@ -3,8 +3,19 @@ package com.xwtech.miaosha.dao;
 import com.xwtech.miaosha.damain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
 
-public interface UserRepository extends JpaSpecificationExecutor<User>, JpaRepository<User,Long> {
+import java.util.List;
 
+public interface UserRepository extends JpaSpecificationExecutor<User>, JpaRepository<User,Long>,Repository<User,Long> {
 
+    @Query(value = "select * from user where nickname =?",nativeQuery = true)
+    List<User> queryUserByName(String name);
+
+    @Query(value = "select * from user where nickname =? and mobile =?",nativeQuery = true)
+    List<User> queryUserByNameAndMobile(String name,String mobile);
+
+    @Query(value = "select * from user where nickname like ? ORDER by id DESC ",nativeQuery = true)
+    List<User> queryUserByNameLike(String name);
 }
